3a468bcbc8d98971830b4d96d195c48dc6b63e75
okoroha <okoroha@coventry.ac.uk>
2023-12-19 23:47:30 +0000

    add cert

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import express, { Express, Request, Response , Application } from 'express';
+import fs from "fs";
+
+const file = fs.readFileSync("./E8A6B107AAFA9A65A739BEA3BB9FBE76.txt")
 
 const app: Application = express();
 const port = process.env.PORT || 8080;
@@ -7,6 +10,10 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
 });
 
+app.get('/.well-known/pki-validation/E8A6B107AAFA9A65A739BEA3BB9FBE76.txt', (req: Request, res: Response) => {
+  res.sendFile('fpl-server/E8A6B107AAFA9A65A739BEA3BB9FBE76.txt');
+
+})
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
 });
