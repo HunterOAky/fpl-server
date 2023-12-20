052a39de241bcc9e50004bff9aee7f0f09525341
okoroha <okoroha@coventry.ac.uk>
2023-12-20 20:55:22 +0000

    rm cert and key

--- index.ts
+++ index.ts
@@ -17,10 +17,6 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
 });
 
-app.get('/.well-known/pki-validation/E8A6B107AAFA9A65A739BEA3BB9FBE76.txt', (req: Request, res: Response) => {
-  res.sendFile('/home/fpl-server/E8A6B107AAFA9A65A739BEA3BB9FBE76.txt');
-
-})
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
 });
